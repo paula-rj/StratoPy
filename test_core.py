f350bb1c9f3e68088a1a8f30e11ba541106fcfa9
georgynio <ragy3008@gmail.com>
2021-09-08 23:50:02 -0300

    sin cambios

--- test_core.py
+++ test_core.py
@@ -1,8 +1,15 @@
+#%%
 from core import CloudClass
 
 y = CloudClass(
     'data/CloudSat/2019003151948_67564_CS_2B-CLDCLASS_GRANULE_P1_R05_E08_F03.hdf'
 )
 
-data = y.read()
-print(y.plot_statistics())
+data = y.read_hdf()
+# y.plot_statistics()
+
+#%%
+# testando algunas funcionalidades
+
+# print(dir(y) )
+print(y.__repr__())
