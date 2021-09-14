210def5c5c1f40c98f44af4f8771bf740573130e
jose <jorobledo@unc.edu.ar>
2021-09-14 16:50:00 -0300

    modificado por black y flake

--- graficar_nubes.py
+++ graficar_nubes.py
@@ -1,11 +1,7 @@
 #%%
 
 
-def plot_cloud(path,
-               ROI=range(12346, 12446),
-               var='CloudLayerType',
-               *args,
-               **kwargs):
+def plot_cloud(path, ROI=range(12346, 12446), var="CloudLayerType", *args, **kwargs):
     from pyhdf.SD import SD, SDC
     from pyhdf.HDF import HDF, HC
     from pyhdf.VS import VS
@@ -13,11 +9,11 @@ def plot_cloud(path,
     from matplotlib.colors import ListedColormap
     import numpy as np
 
-    #metadata
+    # metadata
     hdf_file = HDF(path, HC.READ)
     vs = hdf_file.vstart()
-    lat = vs.attach('Latitude', write=0)[:]
-    lon = vs.attach('Longitude', write=0)[:]
+    lat = vs.attach("Latitude", write=0)[:]
+    lon = vs.attach("Longitude", write=0)[:]
     latitude = np.array(lat).flatten()
     longitude = np.array(lon).flatten()
 
@@ -31,56 +27,58 @@ def plot_cloud(path,
 
     print(plot_data.shape)
     # colors = ['m','r','orange','y','lime','skyblue','blue','indigo','white']
-    colors = [
-        'black', 'indigo', 'blue', 'skyblue', 'lime', 'y', 'orange', 'r', 'm'
-    ]
+    colors = ["black", "indigo", "blue", "skyblue", "lime", "y", "orange", "r", "m"]
     # labels = ['Deep', 'Ns', 'Cu', 'Sc', 'St', 'Ac', 'As', 'High', 'Missing']
-    labels = ['Missing', 'Ci', 'As', 'Ac', 'St', 'Sc', 'Cu', 'Ns', 'DC']
+    labels = ["Missing", "Ci", "As", "Ac", "St", "Sc", "Cu", "Ns", "DC"]
     cmap = ListedColormap(colors)
     ax = plt.subplot(111)
-    psm = ax.imshow(plot_data,
-                    origin='lower',
-                    cmap=cmap,
-                    aspect='auto',
-                    vmin=0,
-                    vmax=10,
-                    extent=[0, plot_data.shape[1], 0, 9],
-                    interpolation='none',
-                    *args,
-                    *kwargs)
+    psm = ax.imshow(
+        plot_data,
+        origin="lower",
+        cmap=cmap,
+        aspect="auto",
+        vmin=0,
+        vmax=10,
+        extent=[0, plot_data.shape[1], 0, 9],
+        interpolation="none",
+        *args,
+        *kwargs,
+    )
 
-    cb = plt.colorbar(psm, orientation='horizontal', ax=ax)
+    cb = plt.colorbar(psm, orientation="horizontal", ax=ax)
     loc = np.arange(0.5, len(labels) + 1, 10 / 9)
     cb.set_ticks(loc)
     cb.set_ticklabels(labels)
-    ax.set_ylabel('Layer')
-    ax.set_xlabel('Latitude')
+    ax.set_ylabel("Layer")
+    ax.set_xlabel("Latitude")
     nticks = 5
     ax.set_xticks([plot_data.shape[1] * i / nticks for i in range(nticks)])
-    ax.set_xticklabels([
-        f'{latitude[int(i * plot_data.shape[1] / nticks)]:.2f}'
-        for i in range(nticks)
-    ])
+    ax.set_xticklabels(
+        [f"{latitude[int(i * plot_data.shape[1] / nticks)]:.2f}" for i in range(nticks)]
+    )
     ax.set_yticks(np.arange(0, 10, 1))
-    plt.grid(axis='y')
+    plt.grid(axis="y")
     return psm
 
 
-#test
+# test
 
 import matplotlib.pyplot as plt
 
 files = [
-    '2019002175851_67551', '2019003052836_67558', '2019003151948_67564',
-    '2019003165820_67565', '2019003183652_67566'
+    "2019002175851_67551",
+    "2019003052836_67558",
+    "2019003151948_67564",
+    "2019003165820_67565",
+    "2019003183652_67566",
 ]
 
 file_to_plot = files[3]
 
-path = f'data/CloudSat/{file_to_plot}_CS_2B-CLDCLASS_GRANULE_P1_R05_E08_F03.hdf'
+path = f"data/CloudSat/{file_to_plot}_CS_2B-CLDCLASS_GRANULE_P1_R05_E08_F03.hdf"
 
 plt.figure(figsize=(7, 5))
-plot_cloud(path, ROI=range(15000, 25000), var='CloudLayerType')
+plot_cloud(path, ROI=range(15000, 25000), var="CloudLayerType")
 plt.show()
 
 # %%
