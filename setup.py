a7571a2060034f2cc3f7975213c546a8fbcc8d15
jose <jorobledo@unc.edu.ar>
2021-09-21 16:09:48 -0300

    cree setup.py y eliminé requirements.txt

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+REQUIREMENTS = ["numpy", "pandas", "matplotlib",
+                "geopandas", "cartopy", "pyhdf", "netcdf"]
+
+setup(
+    name='StratoPy',
+    version='0.1.0',
+    description="",
+    lond_description="",
+    author="Paula Romero",
+    author_email="",
+    url="https://github.com/paula-rj/StratoPy",
+    py_modules=None,
+    packages=["stratopy"],
+    include_package_data=True,
+    license="The MIT License",
+    keywords=["stratopy", "clouds"],
+    classifiers=[
+        "Development Status :: 4 -Beta",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.9"
+    ]
+
+
+)
