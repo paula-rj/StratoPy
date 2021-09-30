3e312831c2ba753f2e57536e7471460390c3ac00
jotavecorta <julianvilla.90@gmail.com>
2021-09-30 15:31:46 -0300

    Cambios en test_core.py: ya no exite el metodo read_hdf

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,3 +1,6 @@
+from stratopy.cloudsat import read_hdf
+
+
 """
 para escribir tests devemos tener en mente,
 
@@ -15,3 +18,8 @@ TDD kant beck - One-step Test
 
 path = "data/CloudSat/\
 2019003151948_67564_CS_2B-CLDCLASS_GRANULE_P1_R05_E08_F03.hdf"
+
+
+def test_shape():
+    data = read_hdf(path)
+    assert data.shape == data.shape
