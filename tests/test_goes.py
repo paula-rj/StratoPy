b2a838471b9656434f1de7ade90d2097db3490b8
jotavecorta <julianvilla.90@gmail.com>
2021-10-18 16:38:50 -0300

    Cambios en GoesDataframe y en sus respectivos test

--- tests/test_goes.py
+++ tests/test_goes.py
@@ -2,23 +2,20 @@ from stratopy import goes
 
 path_channel_3 = (
     "data/GOES16/"
-    "OR_ABI-L2-CMIPF-M3C03_G16_s20190021800363_e20190021811129_\
-    c20190021811205.nc"
+    "OR_ABI-L2-CMIPF-M3C03_G16_s20190021800363_e20190021811129_c20190021811205.nc"
 )
 path_channel_7 = (
     "data/GOES16/"
-    "OR_ABI-L2-CMIPF-M3C07_G16_s20190021800363_e20190021811141_\
-    c20190021811202.nc"
+    "OR_ABI-L2-CMIPF-M3C07_G16_s20190021800363_e20190021811141_c20190021811202.nc"
 )
 path_channel_13 = (
     "data/GOES16/"
-    "OR_ABI-L2-CMIPF-M3C13_G16_s20190021800363_e20190021811141_\
-    c20190021811221.nc"
+    "OR_ABI-L2-CMIPF-M3C13_G16_s20190021800363_e20190021811141_c20190021811221.nc"
 )
 
 
 def test_metadata():
-    data = goes.GoesDataFrame(path_channel_3, path_channel_7, path_channel_13)
+    data = GoesDataFrame(path_channel_3, path_channel_7, path_channel_13)
     repr_val = data.__repr__()
     assert isinstance(repr_val, str)
 
