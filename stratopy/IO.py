b3119ead49783d973950c1e5f79c47aaf120537f
jotavecorta <julianvilla.90@gmail.com>
2021-12-02 14:33:27 -0300

    Agregué fetch_goes

--- stratopy/IO.py
+++ stratopy/IO.py
@@ -8,6 +8,7 @@ from diskcache import Cache
 from diskcache.core import ENOVAL
 
 from .cloudsat import read_hdf
+from .goes import read_nc
 
 # type: ignore
 DEFAULT_CACHE_PATH = pathlib.Path(
@@ -56,3 +57,46 @@ def fetch_cloudsat(
         df = read_hdf(fname)
 
     return df
+
+
+def fetch_goes(
+    dirname,
+    user,
+    passwd,
+    host="ftp.avl.class.noaa.gov",
+    tag="stratopy-goes",
+    path=DEFAULT_CACHE_PATH,
+):
+    """Fetch files of a certain date from GOES server and
+    stores in a local cache.
+    """
+    cache = Cache(path)
+
+    # Transform dirname into cache id
+    id_ = os.path.split(dirname)[-1]
+
+    # Search in local cache
+    cache.expire()
+    result = cache.get(id_, default=ENOVAL, retry=True)
+
+    if result is ENOVAL:
+
+        ftp = FTP()
+        ftp.connect(host=host)
+        ftp.login()
+
+        buffer_file = io.BytesIO()
+        ftp.retrbinary(f"RETR {dirname}", buffer_file.write)
+        result = buffer_file.getvalue()
+
+        cache.set(id_, result, tag=tag)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        fname = os.path.join(tmpdirname, id_)
+
+        with open(fname, "wb") as fp:
+            fp.write(result)
+
+        rgb = read_nc(fname)
+
+    return rgb
