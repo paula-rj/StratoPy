7857b94a6e4b17f9773300ecf239a1dc4c58a710
jotavecorta <julianvilla.90@gmail.com>
2022-02-10 16:31:21 -0300

    agregue test_cache_goes a test_IO.py

--- tests/test_IO.py
+++ tests/test_IO.py
@@ -7,6 +7,7 @@ from diskcache.core import ENOVAL
 
 from stratopy import IO
 from stratopy.cloudsat import CloudSatFrame
+from stratopy.goes import Goes
 
 DEFAULT_CACHE_PATH = pathlib.Path(
     os.path.expanduser(os.path.join("~", "stratopy_cache"))
@@ -26,6 +27,12 @@ PATH_CLOUDSAT = (
 CLOUDSAT_SERVER_DIR = (
     "2B-CLDCLASS.P1_R05/2019/003/"
     "2019003151948_67564_CS_2B-CLDCLASS_GRANULE_P1_R05_E08_F03.hdf"
+)
+
+GOES_SERVER_DIR = (
+    "noaa-goes16/ABI-L2-CMIPF/2019/002/18/"
+    "OR_ABI-L2-CMIPF-M3C03_G16_s20190021800363_e20190021811129_"
+    "c20190021811205.nc"
 )
 
 
@@ -50,6 +57,28 @@ def test_cache_cloudsat():
     assert isinstance(
         cls_frame,
         CloudSatFrame,
+    )
+
+
+def test_cache_goes():
+    # In memory buffer to store binary
+    buffer = io.BytesIO()
+
+    # open file and store
+    with open(PATH_GOES, "rb") as binary_stream:
+        buffer.write(binary_stream.read())
+
+    with mock.patch(
+        "diskcache.Cache.get", return_value=buffer.getvalue()
+    ) as cache_get:
+        goes_frame = IO.fetch_goes(GOES_SERVER_DIR)
+        cache_get.assert_called_with(
+            "20190021800363", default=ENOVAL, retry=True
+        )
+
+    assert isinstance(
+        goes_frame,
+        Goes,
     )
 
 
