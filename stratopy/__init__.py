5b245487f1b8f12c9717f81f0f6f4399cd2fc580
Paula <paula.rjure@gmail.com>
2022-08-18 16:23:52 -0300

    Reformulated folders

--- stratopy/__init__.py
+++ stratopy/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # =============================================================================
@@ -7,26 +7,10 @@
 
 r"""StratoPy Project.
 
-A Python package designed to easily manipulate CloudSat and GOES-R
+A Python package designed to easily manipulate CloudSat and GOES-16
 and generate labeled images containing cloud types.
 
-It consists in five modules:
-
-- stratopy.cloudsat module
-- stratopy.core module
-- stratopy.goes module
-- stratopy.io module
 """
 
 __name__ = "stratopy"
 __version__ = "0.0.1"
-
-
-# =============================================================================
-# IMPORTS
-# =============================================================================
-
-from .cloudsat import *  # noqa
-from .core import *  # noqa
-from .goes import *  # noqa
-from .IO import *  # noqa
