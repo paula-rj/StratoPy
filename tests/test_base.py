8d3de4ebd756cec8cf7b537ad72e01ee297d90cd
Paula <paula.rjure@gmail.com>
2022-09-22 16:21:47 -0300

    added connectorABC test and style changes 2

--- tests/test_base.py
+++ tests/test_base.py
@@ -4,6 +4,7 @@ from stratopy.remote_access import base
 
 
 def test_ConnectorABC():
+
     class FakeConnector(base.ConnectorABC):
         @classmethod
         def get_endpoint(cls):
@@ -12,7 +13,7 @@ def test_ConnectorABC():
         def _makequery(self, endpoint, pdate):
             endpoint.extend(["_makequery", pdate])
             return endpoint
-
+        
         def _download(self, query):
             query.append("_download")
             return query
@@ -21,13 +22,12 @@ def test_ConnectorABC():
             response.append("_parse_result")
             return response
 
+           
     conn = FakeConnector()
     result = conn.fetch("june 25th 2022")
 
-    expected = [
-        "_makequery",
-        datetime.datetime(2022, 6, 25),
-        "_download",
-        "_parse_result",
+    expected = ["_makequery", 
+    datetime.datetime(2022, 6, 25), 
+    "_download", "_parse_result" 
     ]
     assert result == expected
