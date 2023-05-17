5c5d78dc1673ed9f56eb4fb2c0d9dbb43c15fb77
juanbc <jbcabral@unc.edu.ar>
2023-05-17 12:22:47 -0300

    missing constants module

---
+++ stratopy/constants.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# License: MIT (https://tldrlegal.com/license/mit-license)
+# Copyright (c) 2022, Paula Romero Jure et al.
+# All rights reserved.
+
+# =============================================================================
+# META
+# =============================================================================
+
+"""Constants used inside stratopy."""
+
+
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+#:
+STRATOPY_METADATA_KEY = "_STRATOPY_"
+
+
+# ORBITS ======================================================================
+
+#:
+GEOSTATIONARY = "geostationary"
+
+#:
+POLAR = "polar"
+
+#:
+ORBIT_TYPES = (GEOSTATIONARY, POLAR)
